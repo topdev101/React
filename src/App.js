a80893c9ae9c7adf55023677e9357deed31af66a
topdev101 <ritachi0502@gmail.com>
2020-02-05 19:22:09 -0500

    Almost done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Banner from "./components/banner/Banner";
+import Contact from "./components/contact/Contact";
+import Features from "./components/features/Features";
+import Footer from "./components/footer/Footer";
+import FooterBottom from "./components/footer/FooterBottom";
+import Navbar from "./components/navbar/Navbar";
+import Projects from "./components/projects/Projects";
+import Resume from "./components/resume/Resume";
+import Testimonial from "./components/tesimonial/Testimonial";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Portfolio</h1>
-      <h1 className='text-3xl font-bold underline' >Hello world!</h1>
+    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
+        <Navbar />
+      <div className="max-w-screen-xl mx-auto">
+        <Banner />
+        <Features />
+        <Projects />
+        <Resume />
+        <Testimonial />
+        <Contact />
+        <Footer />
+        <FooterBottom />
+      </div>
     </div>
   );
 }
